04afe11212b97fdf3f9038beb7efb94792b6016f
Cedric <cedricsalvadsalva@gmail.com>
2026-03-28 11:38:03 -0600

    Form to sign up and log in created

--- src/Component/Authorization/Login.tsx
+++ src/Component/Authorization/Login.tsx
@@ -1,14 +1,59 @@
+import { useState, type ChangeEvent, type FormEvent } from "react";
+
+interface LoginFormData {
+  email: string;
+  password: string;
+}
+
+const initial_form: LoginFormData = {
+  email: "",
+  password: "",
+};
+
 function Login() {
+  const [formData, setFormData] = useState<LoginFormData>(initial_form);
+
+  const hanldeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const hanldeSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setFormData(initial_form);
+  };
   return (
     <div>
       <h1>Log in</h1>
       <p>And meet all the world!</p>
-      <form className="flex flex-col gap-4 p-[48px] p-4 rounded-md">
+      <form
+        className="flex flex-col gap-4 p-[48px] p-4 rounded-md"
+        onSubmit={hanldeSubmit}
+      >
         <label htmlFor="email">Email</label>
-        <input type="text" />
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={hanldeChange}
+          required
+          className="bg-lime-950 p-2 rounded-md"
+        />
 
         <label htmlFor="password">Password</label>
-        <input type="password" />
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={hanldeChange}
+          required
+          className="bg-lime-950 p-2 rounded-md"
+        />
+
+        <button>Log in</button>
       </form>
       <button>Forgot my password</button>
     </div>
--- src/Component/Authorization/Signup.tsx
+++ src/Component/Authorization/Signup.tsx
@@ -1,24 +1,104 @@
+import { useState, type ChangeEvent, type FormEvent } from "react";
+
+interface SignupFormData {
+  first_name: string;
+  last_name: string;
+  email: string;
+  birth: string;
+  password: string;
+}
+
+const initial_form: SignupFormData = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  birth: "",
+  password: "",
+};
+
 function Signup() {
+  const [formData, setFormData] = useState<SignupFormData>(initial_form);
+
+  const hanldeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "number" ? Number(value) : value,
+    }));
+    console.log(name, value);
+  };
+
+  const hanldeSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setFormData(initial_form);
+  };
+
   return (
     <div>
       <h1>Sign up</h1>
       <p>And start connecting!</p>
 
-      <form className="flex flex-col gap-4 p-[48px] p-4 rounded-md">
+      <form
+        className="flex flex-col gap-4 p-[48px] p-4 rounded-md"
+        onSubmit={hanldeSubmit}
+      >
         <label htmlFor="firstname">First name</label>
-        <input type="text" />
+        <input
+          type="text"
+          id="firstname"
+          name="first_name"
+          value={formData.first_name}
+          onChange={hanldeChange}
+          required
+          className="bg-lime-950 p-2 rounded-md"
+        />
 
         <label htmlFor="lastname">Last name</label>
-        <input type="text" />
+        <input
+          type="text"
+          id="lastname"
+          name="last_name"
+          value={formData.last_name}
+          onChange={hanldeChange}
+          required
+          className="bg-lime-950 p-2 rounded-md"
+        />
 
         <label htmlFor="email">Email address</label>
-        <input type="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={hanldeChange}
+          required
+          className="bg-lime-950 p-2 rounded-md"
+        />
+
+        <label htmlFor="password">Create your password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={hanldeChange}
+          required
+          className="bg-lime-950 p-2 rounded-md"
+        />
 
         <label htmlFor="birthday">Select your birth date</label>
-        <input type="date" />
+        <input
+          type="date"
+          id="birth"
+          name="birth"
+          value={formData.birth}
+          onChange={hanldeChange}
+          required
+          className="bg-lime-950 p-2 rounded-md"
+        />
+        <button>Sign up</button>
       </form>
 
-      <button>Sign up</button>
       <button>Do you already have an account? Log in</button>
     </div>
   );
